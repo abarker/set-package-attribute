d6c0ddb5c0b02a152aded11cb7ff4821b2231979
Allen Barker <Allen.L.Barker@gmail.com>
2016-07-07 16:41:02 -0400

    cleanup and documentation

--- src/set_package_attribute/set_package_attribute.py
+++ src/set_package_attribute/set_package_attribute.py
@@ -12,21 +12,20 @@ run as a script inside a package which uses explicit relative imports.
 
 To use the module, just import it before any of the non-system files, inside
 any module that you want to possibly run as a script.  Nothing else is
-required.  This module needs to be imported before any explicit relative
-imports or any modules which use such imports.  Any previously-set
+required.  This module needs to be imported *before* any explicit relative
+imports, or any modules which use such imports.  Any previously-set
 `__package__` attributes (other than `None`) will be left unchanged.
 
-Note that this module needs to import the package directory containing the
+Note that this module also needs to import the package directory containing the
 script.  A side-effect of this is that any `__init__.py` files in the package
 path down to the script (from the top package level) will be executed.  This
 could give unexpected results, depending on how `__init__.py` files are used in
 a given package.  The effect is essentially the same as if the script file had
 been imported from another module using its fully-qualified module name.
 
-An alternative approach is to always execute a script `module_name` as, for
-example, `python -m pkg_toplevel.pkg_subdir.module_name`.  This requires the
-full package name, however, and a different invocation method than other
-scripts.
+As far as efficiency, only the first import of the module does any real work,
+when it is initialized.  The module initialization only performs relatively
+trivial computations.
 
 Further details
 ---------------
@@ -35,15 +34,37 @@ On initial import this module searches for the module `__main__` in
 `sys.modules`.  If that module is not found then the current runtime was not
 started from a script and nothing is done.  If module `__main__` is found then
 the current runtime was started from a script.  In that case the `__package__`
-attribute for the `__main__` module is computed and set in that module's
-namespace.  (If there already was a `__package__` attribute in the namespace
-then nothing is done.)  The package is then imported under the full package
-name and the module is also added to `sys.modules` under the full package name.
+attribute for the `__main__` module is computed and set in the `__main__`
+module's namespace.  Only the `__main__` module is ever modified.  If there is
+already a `__package__` attribute in the namespace of `__main__` then nothing
+is done.  The containing the `__main__` module is found by going up the
+directory tree, from its source file.
+
+After setting the `__package__` attribute in the `__main__` module the package
+directory containing the `__main__` module is then imported, using its
+fully-qualified package name.  The `__main__` module is also added to
+`sys.modules` under its full package name.
 
 .. seealso:: 
 
     This module is based on the basic method described in the answers on this
     StackOverflow page: http://stackoverflow.com/questions/2943847/
+
+.. note::
+
+    An alternative approach would be to always execute scripts inside packages
+    with the `-m` flag set.  For example, to execute a script `module_name.py`,
+    which is in a subdirectory inside a package `pkg_toplevel`, you would use:
+
+    .. code:: bash
+
+       python -m pkg_toplevel.pkg_subdir.module_name
+       
+    This requires the full package name to be used, however, and has a
+    different invocation method than other scripts.  Also, the directory
+    containing the top-level package directory `pkg_toplevel` (i.e., its parent
+    directory) needs to be in Python's search path for modules in order for
+    this approach to work.
 
 .. note::
 
@@ -100,7 +121,7 @@ def set_package_attribute():
             # Get the package name and set the __package__ variable.
             # Note: the package name does not include the name of the module itself.
             full_package_name = ".".join(reversed(parent_dirs))
-            main_module.__package__ = str(full_package_name)
+            main_module.__package__ = full_package_name
 
             # Now do the actual import of the full package.
             print("\nfull_package_name is", full_package_name)
@@ -112,10 +133,11 @@ def set_package_attribute():
                 # Failure; insert dirname in sys.path, then try the import again.
                 sys.path.insert(1, dirname) # Use 1 instead of 0; 0 is script's dir.
                 package_module = __import__(full_package_name)
+                del sys.path[1] # Remove the added path; no longer needed.
 
             assert full_package_name in sys.modules # True
             # TODO: I added this part... seems like it sets the right alias to the module...
-            full_package_name += "." + str(filename) # Add filename part to the end.
+            full_package_name += "." + str(filename) # Add the filename part to the end.
             assert full_package_name not in sys.modules # True
             sys.modules[full_package_name] = main_module
 
